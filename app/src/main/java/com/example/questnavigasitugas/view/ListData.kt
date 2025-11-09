7656f2cae4fc0196f4d16b5a7303f632d1c9d81c
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-10 01:08:15 +0700

    membuat composable utama ListData dan menambahkan dua parameter tombol navigasi

---
+++ app/src/main/java/com/example/questnavigasitugas/view/ListData.kt
@@ -0,0 +1,26 @@
+package com.example.questnavigasitugas.view
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListData(
+    OnBerandaClick: () -> Unit,
+    OnFormulirClick: () -> Unit
+) {
+    
