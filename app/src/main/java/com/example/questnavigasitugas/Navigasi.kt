65da8c9e0229879188a6b556bed703f60d561ad5
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-10 01:36:08 +0700

    membuat struktur utama aplikasi dengan Scaffold dan NavHost

---
+++ app/src/main/java/com/example/questnavigasitugas/Navigasi.kt
@@ -0,0 +1,33 @@
+package com.example.questnavigasitugas
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasitugas.view.Formulir
+import com.example.questnavigasitugas.view.Home
+import com.example.questnavigasitugas.view.ListData
+
+
+enum class Navigasi {
+    Home,
+    Formulir,
+    Detail
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Home.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
