e9f2bd4dfab70448fc17ac2d44697cfe300b0b89
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-10 01:37:28 +0700

    menambahkan tiga halaman utama (Home, ListData, Formulir)

--- app/src/main/java/com/example/questnavigasitugas/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasitugas/Navigasi.kt
@@ -31,3 +31,36 @@ fun DataApp(
             startDestination = Navigasi.Home.name,
             modifier = Modifier.padding(innerPadding)
         ) {
+
+            composable(route = Navigasi.Home.name) {
+                Home(
+                    OnMasukBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+
+            composable(route = Navigasi.Detail.name) {
+                ListData(
+                    OnFormulirClick = {
+                        navController.navigate(Navigasi.Formulir.name)
+                    },
+                    OnBerandaClick = {
+                        cancelAndBackToHome(navController)
+                    }
+                )
+            }
+
+            composable(route = Navigasi.Formulir.name) {
+                Formulir(
+                    OnBackBtnClick = {
+                        navController.popBackStack(route = Navigasi.Detail.name, inclusive = false)
+                    },
+                    OnSubmitBtnClick = {
+                        navController.popBackStack(route = Navigasi.Detail.name, inclusive = false)
+                    }
+                )
+            }
+        }
+    }
+}
